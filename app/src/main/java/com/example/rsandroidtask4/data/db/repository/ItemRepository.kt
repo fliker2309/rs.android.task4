75e4d408095318ffe401d56940d171a3bf81b29b
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-08-18 00:39:54 +0300

    add:filled ItemRepository.kt

--- app/src/main/java/com/example/rsandroidtask4/data/db/repository/ItemRepository.kt
+++ app/src/main/java/com/example/rsandroidtask4/data/db/repository/ItemRepository.kt
@@ -1,7 +1,20 @@
 package com.example.rsandroidtask4.data.db.repository
 
 import com.example.rsandroidtask4.data.db.dao.ItemDao
+import com.example.rsandroidtask4.data.db.entity.Item
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ItemRepository(private val itemDao: ItemDao) {
+    suspend fun readItemsFromDb(): List<Item> = withContext(Dispatchers.IO) {
+        itemDao.readItemsFromDb()
+    }
 
+    suspend fun getItemByIdFromDb(id: Int): Item = withContext(Dispatchers.IO) {
+        itemDao.getItemByIdFromDb(id)
+    }
+
+    suspend fun insertItemsInDb(items: List<Item>) = withContext(Dispatchers.IO) {
+        itemDao.insertItemsInDb(items)
+    }
 }
--- app/src/main/java/com/example/rsandroidtask4/presentation/mainfragment/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/rsandroidtask4/presentation/mainfragment/MainFragmentViewModel.kt
@@ -1,9 +1,31 @@
 package com.example.rsandroidtask4.presentation.mainfragment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rsandroidtask4.data.db.entity.Item
 import com.example.rsandroidtask4.data.db.repository.ItemRepository
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
 @InternalCoroutinesApi
-class MainFragmentViewModel (private val repository: ItemRepository) : ViewModel(){
+class MainFragmentViewModel(private val repository: ItemRepository) : ViewModel() {
+
+    private var _mutableMainFragmentLiveData: MutableLiveData<List<Item>> =
+        MutableLiveData(emptyList())
+    val mainFragmentLiveData: LiveData<List<Item>>
+        get() = _mutableMainFragmentLiveData
+
+    private var _loadingLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    val loadingLiveData: LiveData<Boolean>
+        get() = _loadingLiveData
+
+    init {
+        viewModelScope.launch {
+            _mutableMainFragmentLiveData.value = repository.
+        }
+    }
+
+
 }
