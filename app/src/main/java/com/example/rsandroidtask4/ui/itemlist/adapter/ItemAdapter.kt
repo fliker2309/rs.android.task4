bc0389367eb9d736ba8be129486350e1c49228e2
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-08-19 18:04:59 +0300

    add:ready ItemAdapter.kt

--- app/src/main/java/com/example/rsandroidtask4/ui/itemlist/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/rsandroidtask4/ui/itemlist/adapter/ItemAdapter.kt
@@ -1,8 +1,50 @@
 package com.example.rsandroidtask4.ui.itemlist.adapter
 
-import androidx.recyclerview.widget.ListAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-/*
+import com.example.rsandroidtask4.data.db.entity.Item
+import com.example.rsandroidtask4.databinding.ViewHolderItemBinding
 
-class ItemAdapter : ListAdapter<DataItem,RecyclerView.ViewHolder>(ItemDiffUtilCallback()){
-}*/
+
+class ItemAdapter() : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    private var items: List<Item> = listOf()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+      holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    fun updateItems(newItems: List<Item>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+
+
+
+    class ItemViewHolder private constructor(private val binding: ViewHolderItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun from(parent: ViewGroup): ItemViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ViewHolderItemBinding.inflate(inflater, parent, false)
+                return ItemViewHolder(binding)
+            }
+        }
+        fun bind(item:Item){
+            binding.apply {
+                ageTv.text = item.age.toString()
+                nameTv.text = item.name
+                breedTv.text = item.breed
+            }
+        }
+    }
+
+}
