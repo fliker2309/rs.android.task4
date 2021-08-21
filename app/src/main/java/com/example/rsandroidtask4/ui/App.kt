57775e8e14d3254defc5d3fd21ebb358deb57872
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-08-22 01:02:39 +0300

    add: App class

---
+++ app/src/main/java/com/example/rsandroidtask4/ui/App.kt
@@ -0,0 +1,22 @@
+package com.example.rsandroidtask4.ui
+
+import android.app.Application
+import android.content.Context
+import com.example.rsandroidtask4.data.db.database.ItemDatabase
+import com.example.rsandroidtask4.data.db.repository.ItemRepository
+import com.example.rsandroidtask4.data.locator.ServiceLocator
+import com.example.rsandroidtask4.data.locator.ServiceLocator.locate
+import kotlinx.coroutines.InternalCoroutinesApi
+
+class App : Application() {
+
+
+    @InternalCoroutinesApi
+    override fun onCreate() {
+        super.onCreate()
+
+        ServiceLocator.register<Context>(this)
+        ServiceLocator.register<ItemDatabase>(ItemDatabase.getDatabase(locate()))
+        ServiceLocator.register(ItemRepository(locate()))
+    }
+}
