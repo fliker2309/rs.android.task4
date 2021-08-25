f94b15ba08b72940bc916803ad4e8631fc1becaf
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-08-25 22:14:54 +0300

    fix: areContentsTheSame

--- app/src/main/java/com/example/rsandroidtask4/ui/itemlist/adapter/ItemDiffUtilCallback.kt
+++ app/src/main/java/com/example/rsandroidtask4/ui/itemlist/adapter/ItemDiffUtilCallback.kt
@@ -4,8 +4,10 @@ import androidx.recyclerview.widget.DiffUtil
 import com.example.rsandroidtask4.data.db.entity.Item
 
 class ItemDiffUtilCallback : DiffUtil.ItemCallback<Item>() {
-    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean = oldItem.id == newItem.id
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean = oldItem == newItem
 
-    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean = oldItem == newItem
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem.id == newItem.id && oldItem.name == newItem.name && oldItem.age == newItem.age && oldItem.breed == newItem.breed
+    }
 
 }
