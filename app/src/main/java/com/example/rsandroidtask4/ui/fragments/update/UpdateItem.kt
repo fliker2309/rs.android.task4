eca50badc575de351cc2468fb95fcc4d2020dfc9
Zhenya <evgeniikapchik@gmail.com>
2021-09-03 21:35:19 +0300

    add UpdateViewModel.kt & UpdateViewModelFactory.kt

--- app/src/main/java/com/example/rsandroidtask4/ui/fragments/update/UpdateItem.kt
+++ app/src/main/java/com/example/rsandroidtask4/ui/fragments/update/UpdateItem.kt
@@ -1,6 +1,21 @@
 package com.example.rsandroidtask4.ui.fragments.update
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.rsandroidtask4.databinding.FragmentUpdateItemBinding
 
 class UpdateItem : Fragment() {
+
+
+    private var binding: FragmentUpdateItemBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View =
+        FragmentUpdateItemBinding.inflate(inflater, container, false).also { binding = it }.root
 }
