c3bb7e75bfc41850d212cad3a8d1856d883214c3
Zhenya <evgeniikapchik@gmail.com>
2021-09-05 01:02:07 +0300

    work navigation between list and UpdateFragment.kt

--- app/src/main/java/com/example/rsandroidtask4/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/rsandroidtask4/ui/fragments/update/UpdateFragment.kt
@@ -6,33 +6,41 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.rsandroidtask4.R
 import com.example.rsandroidtask4.databinding.FragmentUpdateItemBinding
 
 class UpdateFragment : Fragment() {
 
-
-    private var binding: FragmentUpdateItemBinding? = null
+    private val args by navArgs<UpdateFragmentArgs>()
+    private var _binding: FragmentUpdateItemBinding? = null
+    private val binding: FragmentUpdateItemBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View =
-        FragmentUpdateItemBinding.inflate(inflater, container, false).also { binding = it }.root
+    ): View {
+        _binding = FragmentUpdateItemBinding.inflate(inflater, container, false)
+        binding.apply {
+            textUpdateName.setText(args.currentEmployee.name)
+            textUpdateSurname.setText(args.currentEmployee.surname)
+            textUpdateAge.setText(args.currentEmployee.age)
+            textUpdatePosition.setText(args.currentEmployee.position)
+            textUpdateExperience.setText(args.currentEmployee.experience)
+        }
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initListeners()
-
-
-
-
         super.onViewCreated(view, savedInstanceState)
 
     }
 
 
-    private fun <T> views(block: FragmentUpdateItemBinding.() -> T): T? = binding?.block()
+    private fun <T> views(block: FragmentUpdateItemBinding.() -> T): T? = binding.block()
 
     private fun initListeners() {
         views {
