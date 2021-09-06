00f1847d4e559ee532ac9e79662aaa5e460d58c0
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-09-07 02:55:55 +0300

    flow-> liveData in EmployeeRepository.kt

--- app/src/main/java/com/example/rsandroidtask4/data/db/repository/EmployeeRepository.kt
+++ app/src/main/java/com/example/rsandroidtask4/data/db/repository/EmployeeRepository.kt
@@ -1,5 +1,6 @@
 package com.example.rsandroidtask4.data.db.repository
 
+import androidx.lifecycle.LiveData
 import com.example.rsandroidtask4.data.db.database.EmployeeDatabase
 import com.example.rsandroidtask4.data.db.entity.Employee
 import kotlinx.coroutines.flow.Flow
@@ -9,13 +10,13 @@ class EmployeeRepository(private val employeeDatabase: EmployeeDatabase) {
 
     private val employeeDao get() = employeeDatabase.itemDao()
 
-    fun getEmployees(): Flow<List<Employee>> = employeeDao.getEmployees()
+    fun getEmployees(): LiveData<List<Employee>> = employeeDao.getEmployees()
 
-    fun sortEmployeesByAge(): Flow<List<Employee>> = employeeDao.sortEmployeesByAge()
-    fun sortEmployeesByName(): Flow<List<Employee>> = employeeDao.sortEmployeesByName()
-    fun sortEmployeesByPosition(): Flow<List<Employee>> = employeeDao.sortEmployeesByPosition()
-    fun sortEmployeesByExperience(): Flow<List<Employee>> = employeeDao.sortEmployeesByExperience()
-    fun sortEmployeesBySurname(): Flow<List<Employee>> = employeeDao.sortEmployeeBySurname()
+    fun sortEmployeesByAge():  LiveData<List<Employee>> = employeeDao.sortEmployeesByAge()
+    fun sortEmployeesByName(): LiveData<List<Employee>> = employeeDao.sortEmployeesByName()
+    fun sortEmployeesByPosition():  LiveData<List<Employee>> = employeeDao.sortEmployeesByPosition()
+    fun sortEmployeesByExperience():  LiveData<List<Employee>> = employeeDao.sortEmployeesByExperience()
+    fun sortEmployeesBySurname(): LiveData<List<Employee>> = employeeDao.sortEmployeeBySurname()
 
     suspend fun insertEmployee(employee: Employee) = employeeDao.insertEmployee(employee)
 
