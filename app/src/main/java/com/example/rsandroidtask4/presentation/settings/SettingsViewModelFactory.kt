b0d91e72b1d6d61e6174d012c0df929f9c7eb4b6
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-09-07 22:14:19 +0300

    add:settingsViewModelFactory

--- app/src/main/java/com/example/rsandroidtask4/presentation/settings/SettingsViewModelFactory.kt
+++ app/src/main/java/com/example/rsandroidtask4/presentation/settings/SettingsViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.example.rsandroidtask4.presentation.settings
 
-class SettingsViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class SettingsViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T = when (modelClass) {
+        SettingsViewModel::class.java -> SettingsViewModel()
+        else -> throw IllegalArgumentException("$modelClass is not registered ViewModel")
+    } as T
 }
