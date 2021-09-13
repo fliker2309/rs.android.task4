18755e3e360e5f54ed7411194196b4153830b351
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-09-13 12:20:02 +0300

    fix:app.kt

--- app/src/main/java/com/example/rsandroidtask4/ui/App.kt
+++ app/src/main/java/com/example/rsandroidtask4/ui/App.kt
@@ -9,18 +9,18 @@ import com.example.rsandroidtask4.data.locator.ServiceLocator
 import com.example.rsandroidtask4.data.locator.ServiceLocator.locate
 import kotlinx.coroutines.InternalCoroutinesApi
 
+@InternalCoroutinesApi
 class App : Application() {
 
-    @InternalCoroutinesApi
+
     private val employeeDatabase by lazy { EmployeeDatabase.getDatabase(this) }
     private val sqLiteDatabase by lazy { EmployeeDatabaseCursor.getSQLDatabase(this) }
+    val repository by lazy { EmployeeRepository(employeeDatabase.itemDao(),sqLiteDatabase) }
 
-    @InternalCoroutinesApi
     override fun onCreate() {
         super.onCreate()
 
         ServiceLocator.register<Context>(this)
         ServiceLocator.register(EmployeeDatabase.getDatabase(locate()))
-       /* ServiceLocator.register(EmployeeRepository(locate()))*/
     }
 }
