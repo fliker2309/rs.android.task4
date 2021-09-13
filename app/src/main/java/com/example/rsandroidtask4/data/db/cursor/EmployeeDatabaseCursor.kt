39c93fd8a9fc9829939ad3fdc26686cf45c88def
fliker2309 <60140529+fliker2309@users.noreply.github.com>
2021-09-13 11:50:55 +0300

    fix:EmployeeDatabaseCursor

--- app/src/main/java/com/example/rsandroidtask4/data/db/cursor/EmployeeDatabaseCursor.kt
+++ app/src/main/java/com/example/rsandroidtask4/data/db/cursor/EmployeeDatabaseCursor.kt
@@ -1,4 +1,3 @@
-/*
 package com.example.rsandroidtask4.data.db.cursor
 
 import android.annotation.SuppressLint
@@ -7,12 +6,10 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
-import com.example.rsandroidtask4.data.db.dao.EmployeeDao
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
 import com.example.rsandroidtask4.data.db.entity.Employee
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.Flow
 import java.sql.SQLException
 
 private const val TAG = "SQLiteOpenHelper"
@@ -32,9 +29,13 @@ private const val CREATE_TABLE_SQL =
 
 
 class EmployeeDatabaseCursor(context: Context) : SQLiteOpenHelper(
-    context, DATABASE_NAME, null,
+    context,
+    DATABASE_NAME,
+    null,
     DATABASE_VERSION
-), EmployeeDao {
+) {
+    private val database get() = requireNotNull(INSTANCE)
+
     override fun onCreate(db: SQLiteDatabase?) {
         try {
             db?.execSQL(CREATE_TABLE_SQL)
@@ -47,15 +48,9 @@ class EmployeeDatabaseCursor(context: Context) : SQLiteOpenHelper(
         Log.d(TAG, "onUpgrade")
     }
 
-    override fun getEmployees(): LiveData<List<Employee>> {
-        return liveData <List<Employee>> {
-            emit(getEmployeeList())
-        }
-    }
-
-    override suspend fun insertEmployee(employee: Employee) {
+    suspend fun insertEmployee(employee: Employee) {
         Log.d(TAG, "Cursor insertEmployee($employee)")
-        val db = writableDatabase
+
         val values = ContentValues()
         values.put("id", employee.id)
         values.put("name", employee.name)
@@ -63,13 +58,11 @@ class EmployeeDatabaseCursor(context: Context) : SQLiteOpenHelper(
         values.put("age", employee.age)
         values.put("position", employee.position)
         values.put("experience", employee.experience)
-        db.insert(TABLE_NAME, null, values)
-        db.close()
+        database.writableDatabase.insert(TABLE_NAME, null, values)
     }
 
-    override suspend fun updateEmployee(employee: Employee) {
+    suspend fun updateEmployee(employee: Employee) {
         Log.d(TAG, "Cursor updateEmployee($employee)")
-        val db = writableDatabase
         val values = ContentValues()
         values.put("id", employee.id)
         values.put("name", employee.name)
@@ -77,90 +70,74 @@ class EmployeeDatabaseCursor(context: Context) : SQLiteOpenHelper(
         values.put("age", employee.age)
         values.put("position", employee.position)
         values.put("experience", employee.experience)
-        db.update(TABLE_NAME, values, "id" + "=?", arrayOf(employee.id.toString()))
-        db.close()
+        database.writableDatabase.update(
+            TABLE_NAME,
+            values,
+            "id" + "=?",
+            arrayOf(employee.id.toString())
+        )
     }
 
-    override suspend fun deleteEmployee(employee: Employee) {
-        val db = writableDatabase
+    suspend fun deleteEmployee(employee: Employee) {
         Log.d(TAG, "Cursor deleteEmployee($employee)")
-        db.delete(TABLE_NAME, "id" + "=?", arrayOf(employee.id.toString()))
-        db.close()
+        database.writableDatabase.delete(TABLE_NAME, "id" + "=?", arrayOf(employee.id.toString()))
     }
-
-    override fun sortEmployeesByAge(): LiveData<List<Employee>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun sortEmployeesByName(): LiveData<List<Employee>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun sortEmployeeBySurname(): LiveData<List<Employee>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun sortEmployeesByPosition(): LiveData<List<Employee>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun sortEmployeesByExperience(): LiveData<List<Employee>> {
-        TODO("Not yet implemented")
-    }
-
-    */
-/* override fun sortEmployees(order: String): LiveData<List<Employee>> {
-         TODO("Not yet implemented")
-     }*//*
 
 
     @SuppressLint("Range")
-    private suspend fun getEmployeeList(): List<Employee> {
-        return withContext(Dispatchers.IO) {
-            val listOfEmployees = mutableListOf<Employee>()
-            val db = readableDatabase
-            val selectQuery = "SELECT * FROM $TABLE_NAME"
-            val cursor = db.rawQuery(selectQuery,null)
-            try {
-                cursor.let {
-                    if (cursor.moveToFirst()) {
-                        do {
-                            val id = cursor.getInt(cursor.getColumnIndex("id"))
-                            val name = cursor.getString(cursor.getColumnIndex("name"))
-                            val surname = cursor.getString(cursor.getColumnIndex("surname"))
-                            val age = cursor.getString(cursor.getColumnIndex("age"))
-                            val position = cursor.getString(cursor.getColumnIndex("position"))
-                            val experience = cursor.getString(cursor.getColumnIndex("experience"))
-                            listOfEmployees.add(
-                                Employee(
-                                    id,
-                                    name,
-                                    surname,
-                                    age,
-                                    position,
-                                    experience
-                                )
-                            )
-                        } while (cursor.moveToNext())
-                    }
-                }
+    suspend fun getEmployeeList(): Flow<List<Employee>> {
 
-            } catch (exception: SQLException) {
-                Log.e(TAG, "Cursor", exception)
-            } finally {
-                cursor.close()
+        val listOfEmployees = mutableListOf<Employee>()
+        val liveDataEmployees = MutableLiveData<List<Employee>>()
+        val db = readableDatabase
+        val selectQuery = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(selectQuery, null)
+        try {
+            cursor.let {
+                if (cursor.moveToFirst()) {
+                    do {
+                        val id = cursor.getInt(cursor.getColumnIndex("id"))
+                        val name = cursor.getString(cursor.getColumnIndex("name"))
+                        val surname = cursor.getString(cursor.getColumnIndex("surname"))
+                        val age = cursor.getString(cursor.getColumnIndex("age"))
+                        val position = cursor.getString(cursor.getColumnIndex("position"))
+                        val experience = cursor.getString(cursor.getColumnIndex("experience"))
+                        listOfEmployees.add(
+                            Employee(
+                                id,
+                                name,
+                                surname,
+                                age,
+                                position,
+                                experience
+                            )
+                        )
+                    } while (cursor.moveToNext())
+                }
             }
 
-            listOfEmployees
+        } catch (exception: SQLException) {
+            Log.e(TAG, "Cursor", exception)
+        } finally {
+            cursor.close()
         }
-
+        Log.d(TAG, "Get all fun getEmployeeList")
+        liveDataEmployees.value = listOfEmployees
+        return liveDataEmployees.asFlow()
     }
 
+    companion object
 
-   */
-/* private fun getCursorWithEmployees(): Cursor {
-        return writableDatabase.rawQuery("SELECT * FROM $TABLE_NAME ", null)
-    }*//*
+    @Volatile
+    private var INSTANCE: EmployeeDatabaseCursor? = null
 
+    fun getSQLDatabase(context: Context): EmployeeDatabaseCursor {
+        Log.d(TAG, "Get SQLDatabase")
+        return INSTANCE ?: synchronized(this) {
+            val instance = EmployeeDatabaseCursor(context)
+            INSTANCE = instance
+            instance
+        }
+    }
 
-}*/
+}
